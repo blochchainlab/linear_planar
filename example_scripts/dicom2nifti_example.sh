e7a8581fa924f9569f5f0bd55821f12be6ed5826
Michael Paquette <jotha885@gmail.com>
2020-02-11 11:41:43 +0100

    dummy dicom2nifti script

---
+++ example_scripts/dicom2nifti_example.sh
@@ -0,0 +1,15 @@
+# basic hardcoded Dicom to Nifti script using mrtrix-suite
+
+
+FOLDER=/path/to/data/
+
+
+# non-diffusion data with "topup" export flag
+# the stride flag is extremely important
+mrconvert $FOLDER'dicoms/' $FOLDER'data.nii' -stride 1,2,3 -datatype float64 -nthreads 6 -export_pe_table $FOLDER'PE.txt'
+
+
+# diffusion data with "topup+eddy" export flag and bvec/bval export flag
+# the stride flag is extremely important
+mrconvert $FOLDER'dicoms/' $FOLDER'data.nii' -stride 1,2,3,4 -datatype float64 -nthreads 6 -export_pe_eddy $FOLDER'PE.txt' $FOLDER'eddy_indices.txt' -fslgrad $FOLDER'bvec.txt' $FOLDER'bval.txt'
+
